432949add4f1f945b8ef8127178c4ae501d0a838
OkuhleM <lindokuhlea38@gmail.com>
2021-04-28 14:37:47 +0200

    commited work

--- Grasshopper - Variable Assignment Debug/index.js
+++ Grasshopper - Variable Assignment Debug/index.js
@@ -1,5 +1,14 @@
-var a = "dev"
-var b = "Lab"
+// var a = "dev"
+// var b = "Lab"
 
-var name = a + b;
-console.log(a);
+// var name = a + b;
+// console.log(a);
+
+function apple(x){
+if (x > 1000) 
+  return "It's hotter than the sun!!",
+  
+  else {
+    return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+  }
+};
---
+++ remove first and last letter/index.js
@@ -0,0 +1,6 @@
+function removeChar(str) {
+  const array = str.split("");
+  let res = "";
+  for (let i = 1; i < array.length - 1; i++) res += array[i];
+  return res;
+}
